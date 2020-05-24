8de8bf7ef25b28dc72ca63ab6241c25c92840806
Magdalena Pikul <magdalena.anna.pikul@gmail.com>
2020-05-24 14:00:09 +0200

    Copied the include of bubble.c file into composite.c file; while(1) part of the main function still needs to be transfered

--- gyro_mouse_src/sources/composite.c
+++ gyro_mouse_src/sources/composite.c
@@ -65,6 +65,13 @@
 #endif
 
 #include "pin_mux.h"
+/*******************************************************************************/
+#include "math.h"
+#include "fsl_mma.h"
+#include "fsl_tpm.h"
+#include "fsl_common.h"
+#include "fsl_gpio.h"
+#include "fsl_port.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -88,6 +95,27 @@
 volatile uint8_t shouldToggleTsiChannel = false;
 
 volatile enum operation typeOfOperation = none;
+/*******************************************************************************/
+#define BOARD_TIMER_BASEADDR TPM0
+#define BOARD_FIRST_TIMER_CHANNEL 5U
+#define BOARD_SECOND_TIMER_CHANNEL 2U
+/* Get source clock for TPM driver */
+#define BOARD_TIMER_SOURCE_CLOCK CLOCK_GetFreq(kCLOCK_BusClk)
+#define TIMER_CLOCK_MODE 1U
+/* I2C source clock */
+#define I2C_BAUDRATE 100000U
+
+#define I2C_RELEASE_SDA_PORT PORTE
+#define I2C_RELEASE_SCL_PORT PORTE
+#define I2C_RELEASE_SDA_GPIO GPIOE
+#define I2C_RELEASE_SDA_PIN 25U
+#define I2C_RELEASE_SCL_GPIO GPIOE
+#define I2C_RELEASE_SCL_PIN 24U
+#define I2C_RELEASE_BUS_COUNT 100U
+/* Upper bound and lower bound angle values */
+#define ANGLE_UPPER_BOUND 85U
+#define ANGLE_LOWER_BOUND 5U
+
 
 /*******************************************************************************
  * Prototypes
@@ -96,6 +124,8 @@ volatile enum operation typeOfOperation = none;
 static usb_status_t USB_DeviceCallback(usb_device_handle handle, uint32_t event,
 		void *param);
 static void USB_DeviceApplicationInit(void);
+
+void BOARD_I2C_ReleaseBus(void);
 
 /*******************************************************************************
  * Variables
@@ -128,6 +158,9 @@ usb_device_class_config_list_struct_t g_UsbDeviceCompositeConfigList = {
 };
 
 tsi_calibration_data_t buffer;
+
+/* MMA8451 device address */
+const uint8_t g_accel_address[] = {0x1CU, 0x1DU, 0x1EU, 0x1FU};
 /*******************************************************************************
  * Code
  ******************************************************************************/
@@ -375,6 +408,104 @@ void TSI0_IRQHandler(void) {
 
 	shouldToggleTsiChannel = true;
 }
+/*******************************************************************************/
+static void i2c_release_bus_delay(void)
+{
+    uint32_t i = 0;
+    for (i = 0; i < I2C_RELEASE_BUS_COUNT; i++)
+    {
+        __NOP();
+    }
+}
+
+void BOARD_I2C_ReleaseBus(void)
+{
+    uint8_t i = 0;
+    gpio_pin_config_t pin_config;
+    port_pin_config_t i2c_pin_config = {0};
+
+    /* Config pin mux as gpio */
+    i2c_pin_config.pullSelect = kPORT_PullUp;
+    i2c_pin_config.mux = kPORT_MuxAsGpio;
+
+    pin_config.pinDirection = kGPIO_DigitalOutput;
+    pin_config.outputLogic = 1U;
+    CLOCK_EnableClock(kCLOCK_PortE);
+    PORT_SetPinConfig(I2C_RELEASE_SCL_PORT, I2C_RELEASE_SCL_PIN, &i2c_pin_config);
+    PORT_SetPinConfig(I2C_RELEASE_SDA_PORT, I2C_RELEASE_SDA_PIN, &i2c_pin_config);
+
+    GPIO_PinInit(I2C_RELEASE_SCL_GPIO, I2C_RELEASE_SCL_PIN, &pin_config);
+    GPIO_PinInit(I2C_RELEASE_SDA_GPIO, I2C_RELEASE_SDA_PIN, &pin_config);
+
+    /* Drive SDA low first to simulate a start */
+    GPIO_PinWrite(I2C_RELEASE_SDA_GPIO, I2C_RELEASE_SDA_PIN, 0U);
+    i2c_release_bus_delay();
+
+    /* Send 9 pulses on SCL and keep SDA high */
+    for (i = 0; i < 9; i++)
+    {
+        GPIO_PinWrite(I2C_RELEASE_SCL_GPIO, I2C_RELEASE_SCL_PIN, 0U);
+        i2c_release_bus_delay();
+
+        GPIO_PinWrite(I2C_RELEASE_SDA_GPIO, I2C_RELEASE_SDA_PIN, 1U);
+        i2c_release_bus_delay();
+
+        GPIO_PinWrite(I2C_RELEASE_SCL_GPIO, I2C_RELEASE_SCL_PIN, 1U);
+        i2c_release_bus_delay();
+        i2c_release_bus_delay();
+    }
+
+    /* Send stop */
+    GPIO_PinWrite(I2C_RELEASE_SCL_GPIO, I2C_RELEASE_SCL_PIN, 0U);
+    i2c_release_bus_delay();
+
+    GPIO_PinWrite(I2C_RELEASE_SDA_GPIO, I2C_RELEASE_SDA_PIN, 0U);
+    i2c_release_bus_delay();
+
+    GPIO_PinWrite(I2C_RELEASE_SCL_GPIO, I2C_RELEASE_SCL_PIN, 1U);
+    i2c_release_bus_delay();
+
+    GPIO_PinWrite(I2C_RELEASE_SDA_GPIO, I2C_RELEASE_SDA_PIN, 1U);
+    i2c_release_bus_delay();
+}
+/* Initialize timer module */
+static void Timer_Init(void)
+{
+    /* convert to match type of data */
+    tpm_config_t tpmInfo;
+    tpm_chnl_pwm_signal_param_t tpmParam[2];
+
+    /* Configure tpm params with frequency 24kHZ */
+    tpmParam[0].chnlNumber = (tpm_chnl_t)BOARD_FIRST_TIMER_CHANNEL;
+    tpmParam[0].level = kTPM_LowTrue;
+    tpmParam[0].dutyCyclePercent = 0U;
+
+    tpmParam[1].chnlNumber = (tpm_chnl_t)BOARD_SECOND_TIMER_CHANNEL;
+    tpmParam[1].level = kTPM_LowTrue;
+    tpmParam[1].dutyCyclePercent = 0U;
+
+    /* Initialize TPM module */
+    TPM_GetDefaultConfig(&tpmInfo);
+    TPM_Init(BOARD_TIMER_BASEADDR, &tpmInfo);
+
+    CLOCK_SetTpmClock(1U);
+
+    TPM_SetupPwm(BOARD_TIMER_BASEADDR, tpmParam, 2U, kTPM_EdgeAlignedPwm, 24000U, BOARD_TIMER_SOURCE_CLOCK);
+    TPM_StartTimer(BOARD_TIMER_BASEADDR, kTPM_SystemClock);
+}
+
+/* Update the duty cycle of an active pwm signal */
+static void Board_UpdatePwm(uint16_t x, uint16_t y)
+{
+    /* Updated duty cycle */
+    TPM_UpdatePwmDutycycle(BOARD_TIMER_BASEADDR, (tpm_chnl_t)BOARD_FIRST_TIMER_CHANNEL, kTPM_EdgeAlignedPwm, x);
+    TPM_UpdatePwmDutycycle(BOARD_TIMER_BASEADDR, (tpm_chnl_t)BOARD_SECOND_TIMER_CHANNEL, kTPM_EdgeAlignedPwm, y);
+}
+
+
+
+
+
 
 /*!
  * @brief Main function
@@ -386,6 +517,21 @@ int main(void)
 void main(void)
 #endif
 {
+	mma_handle_t mmaHandle = {0};
+    mma_data_t sensorData = {0};
+    mma_config_t config = {0}; 
+    status_t result; 
+    uint8_t sensorRange = 0;
+    uint8_t dataScale = 0;
+    int16_t xData = 0;
+    int16_t yData = 0;
+    int16_t xAngle = 0;
+    int16_t yAngle = 0;
+    int16_t xDuty = 0;
+    int16_t yDuty = 0;
+    uint8_t idx = 0;
+    uint8_t array_addr_size = 0;
+    
 	volatile uint32_t i = 0;
 	tsi_config_t tsiConfig_normal = { 0 };
 	lptmr_config_t lptmrConfig;
@@ -393,6 +539,8 @@ void main(void)
 
 	BOARD_InitPins();
 	BOARD_BootClockRUN();
+	BOARD_I2C_ReleaseBus();
+    BOARD_I2C_ConfigurePins();
 	BOARD_InitDebugConsole();
 
 	USB_DeviceApplicationInit();
@@ -452,6 +600,57 @@ void main(void)
 
 	//setting default operation to doing nothing
 	typeOfOperation = none;
+	
+	/* I2C initialize */
+    BOARD_Accel_I2C_Init();
+    /* Configure the I2C function */
+    config.I2C_SendFunc = BOARD_Accel_I2C_Send;
+    config.I2C_ReceiveFunc = BOARD_Accel_I2C_Receive;
+
+    /* Initialize sensor devices */
+    array_addr_size = sizeof(g_accel_address) / sizeof(g_accel_address[0]);
+    for (idx = 0; idx < array_addr_size; idx++)
+    {
+        config.slaveAddress = g_accel_address[idx];
+        /* Initialize accelerometer sensor */
+        result = MMA_Init(&mmaHandle, &config);
+        if (result == kStatus_Success)
+        {
+            break;
+        }
+    }
+
+    if (result != kStatus_Success)
+    {
+        PRINTF("\r\nSensor device initialize failed!\r\n");
+        return -1;
+    }
+    /* Get sensor range */
+    if (MMA_ReadReg(&mmaHandle, kMMA8451_XYZ_DATA_CFG, &sensorRange) != kStatus_Success)
+    {
+        return -1;
+    }
+    if (sensorRange == 0x00)
+    {
+        dataScale = 2U;
+    }
+    else if (sensorRange == 0x01)
+    {
+        dataScale = 4U;
+    }
+    else if (sensorRange == 0x10)
+    {
+        dataScale = 8U;
+    }
+    else
+    {
+    }
+    /* Init timer */
+    Timer_Init();
+
+    /* Print a note to terminal */
+    PRINTF("\r\nWelcome to the BUBBLE example\r\n");
+    PRINTF("\r\nYou will see angle data change in the console when change the angles of board\r\n");
 
 	while (1U) {
 #if USB_DEVICE_CONFIG_USE_TASK
